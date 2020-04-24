ac1cbe468e1b5b36cfa0db4e52eccf00787e77dc
Brian Schlenker <bschlenk@umich.edu>
2020-04-24 09:21:30 -0400

    test: set jest coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
